3c1c67577ca09c920c3a4a1df84820d3da80dc61
Mark Junker <m.junker@fubar-dev.de>
2016-04-25 17:17:07 +0200

    Das Versions-Suffix kann jetzt angegeben werden

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ set BUILD_CFG=%1
 if "%BUILD_CFG%" equ "" set BUILD_CFG=Debug
 dotnet restore
 if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard*/project.json -c %BUILD_CFG%
+dotnet build src/Dataline.ExtraStandard*/project.json -c %BUILD_CFG% --version-suffix "%2"
