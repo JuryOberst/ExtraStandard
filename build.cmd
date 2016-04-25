d68cb68cbe92ecc48806d6fef9e9ec4bf19b04bb
Mark Junker <m.junker@fubar-dev.de>
2016-04-25 17:11:10 +0200

    Vereinfachung

--- build.cmd
+++ build.cmd
@@ -3,15 +3,4 @@ set BUILD_CFG=%1
 if "%BUILD_CFG%" equ "" set BUILD_CFG=Debug
 dotnet restore
 if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard -c %BUILD_CFG%
-if %errorlevel% neq  0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard.Extra*/project.json -c %BUILD_CFG%
-if %errorlevel% neq  0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard.Validation*/project.json -c %BUILD_CFG%
-if %errorlevel% neq  0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard.GkvKomServer*/project.json -c %BUILD_CFG%
-if %errorlevel% neq  0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard.DrvKomServer*/project.json -c %BUILD_CFG%
-if %errorlevel% neq  0 exit /b %errorlevel%
-dotnet build test/Dataline.*.Tests/project.json -c %BUILD_CFG%
-if %errorlevel% neq  0 exit /b %errorlevel%
+dotnet build src/Dataline.ExtraStandard*/project.json -c %BUILD_CFG%
