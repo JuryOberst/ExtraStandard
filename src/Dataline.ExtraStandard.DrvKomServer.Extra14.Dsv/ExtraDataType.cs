36c516d2881bd62b2ece42e2185e39ffcda98a01
Mark Junker <m.junker@fubar-dev.de>
2016-08-01 10:10:35 +0200

    Eigener ExtraDataType für VSNR-Anfrage

---
+++ src/Dataline.ExtraStandard.DrvKomServer.Extra14.Dsv/ExtraDataType.cs
@@ -0,0 +1,23 @@
+﻿namespace Dataline.ExtraStandard.DrvKomServer.Extra14.Dsv
+{
+    /// <summary>
+    /// Identifikatoren für Datentypen
+    /// </summary>
+    public static class ExtraDataType
+    {
+        /// <summary>
+        /// Senden einer VSNR-Anfrage
+        /// </summary>
+        public static readonly string VSNRAnfrage = "http://www.extra-standard.de/datatypes/VSNRAnfrage";
+
+        /// <summary>
+        /// Daten-Abfrage
+        /// </summary>
+        public static readonly string DataRequest = "http://www.extra-standard.de/datatypes/DataRequest";
+
+        /// <summary>
+        /// Empfangsbestätigung
+        /// </summary>
+        public static readonly string ConfirmationOfReceipt = "http://www.extra-standard.de/datatypes/ConfirmationOfReceipt";
+    }
+}
