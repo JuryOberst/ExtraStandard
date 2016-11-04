8767524f2fd861d41a88b39fb311bc64ee4a633c
Mark Junker <m.junker@fubar-dev.de>
2016-11-04 12:25:40 +0100

    Ein Kompressions-Handler für "Keine Kompression"

---
+++ src/Dataline.ExtraStandard.Compression/NoneCompressionHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace ExtraStandard.Compression
+{
+    /// <summary>
+    /// Keine Kompression
+    /// </summary>
+    public class NoneCompressionHandler : IExtraCompressionHandler
+    {
+        /// <inheritdoc />
+        public string AlgorithmId { get; } = ExtraCompression.None;
+
+        /// <inheritdoc />
+        public byte[] Compress(byte[] data, string dataName)
+        {
+            return data;
+        }
+
+        /// <inheritdoc />
+        public byte[] Decompress(byte[] data)
+        {
+            return data;
+        }
+    }
+}
