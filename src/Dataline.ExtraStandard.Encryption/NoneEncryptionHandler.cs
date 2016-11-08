075b3eec8a0438c4490bf4a5864c2d8f34a16938
Mark Junker <m.junker@fubar-dev.de>
2016-11-08 13:29:03 +0100

    "Keine Verschlüsselung" als Verschlüsselungsverfahren hinzugefügt

---
+++ src/Dataline.ExtraStandard.Encryption/NoneEncryptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtraStandard.Encryption
+{
+    /// <summary>
+    /// Handler für "Keine Verschlüsselung/Signatur"
+    /// </summary>
+    public class NoneEncryptionHandler : IExtraEncryptionHandler
+    {
+        /// <inheritdoc />
+        public string AlgorithmId { get; } = ExtraEncryption.None;
+
+        /// <inheritdoc />
+        public byte[] Encrypt(byte[] data, DateTime requestTimestamp)
+        {
+            return data;
+        }
+
+        /// <inheritdoc />
+        public byte[] Decrypt(byte[] data)
+        {
+            return data;
+        }
+    }
+}
