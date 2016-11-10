dd407cb35cda58a6090d988049976716af173e6b
Mark Junker <m.junker@fubar-dev.de>
2016-11-10 15:12:16 +0100

    Verwendung nativer Methoden (sofern verfügbar) für die Signatur und Entschlüsselung

---
+++ src/Dataline.ExtraStandard.Encryption/NativePkcs7EncryptionHandler.cs
@@ -0,0 +1,110 @@
+﻿#if NET45
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+using Org.BouncyCastle.Cms;
+
+using X509Certificate = Org.BouncyCastle.X509.X509Certificate;
+
+namespace ExtraStandard.Encryption
+{
+    /// <summary>
+    /// Signatur und Verschlüsselung über PKCS#7 v1.5
+    /// </summary>
+    public class NativePkcs7EncryptionHandler : IExtraEncryptionHandler
+    {
+        private static readonly Oid _hashSha1 = new Oid("SHA1");
+        private static readonly Oid _hashSha256 = new Oid("SHA256");
+
+        private readonly X509Certificate2Collection _allSenderCertificates;
+        private readonly X509Certificate2 _senderCertificate;
+        private readonly Lazy<X509Certificate> _encryptionCertificate;
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="NativePkcs7EncryptionHandler"/> Klasse.
+        /// </summary>
+        /// <param name="senderCertificate">Das X509-Zertifikat des Absenders (PKCS#12)</param>
+        /// <param name="receiverCertificate">Das X509-Zertifikat des Empfängers</param>
+        /// <param name="oldSenderCertificates">Die abgelaufenen X509-Zertifikate des Absenders</param>
+        public NativePkcs7EncryptionHandler(X509Certificate2 senderCertificate, X509Certificate2 receiverCertificate, IEnumerable<X509Certificate2> oldSenderCertificates = null)
+        {
+            _senderCertificate = senderCertificate;
+            _allSenderCertificates = new X509Certificate2Collection((oldSenderCertificates ?? new X509Certificate2[0]).Concat(new[] { senderCertificate }).Reverse().ToArray());
+            _encryptionCertificate = new Lazy<X509Certificate>(() => new Org.BouncyCastle.X509.X509CertificateParser().ReadCertificate(receiverCertificate.RawData));
+        }
+
+        /// <inheritdoc />
+        public string AlgorithmId { get; } = ExtraEncryption.Pkcs7;
+
+        /// <inheritdoc />
+        public byte[] Encrypt(byte[] data, DateTime requestTimestamp)
+        {
+            var signedData = SignData(data, _senderCertificate, requestTimestamp);
+            var encryptedData = EncryptData(signedData, _encryptionCertificate.Value);
+            return encryptedData;
+        }
+
+        /// <inheritdoc />
+        public byte[] Decrypt(byte[] data)
+        {
+            try
+            {
+                var env = new System.Security.Cryptography.Pkcs.EnvelopedCms();
+                env.Decode(data);
+                env.Decrypt(_allSenderCertificates);
+
+                var decryptedData = env.ContentInfo.Content;
+                var sig = new System.Security.Cryptography.Pkcs.SignedCms();
+                sig.Decode(decryptedData);
+                sig.CheckSignature(true);
+
+                var verifiedData = sig.ContentInfo.Content;
+
+                return verifiedData;
+            }
+            catch (Exception ex)
+            {
+                throw new ExtraEncryptionException("No certificate for decryption found.", ex);
+            }
+        }
+
+        private static byte[] SignData(byte[] data, X509Certificate2 signCertificate, DateTime? requestTimestamp = null)
+        {
+            var contentInfo = new System.Security.Cryptography.Pkcs.ContentInfo(data);
+            var signedCms = new System.Security.Cryptography.Pkcs.SignedCms(contentInfo);
+            var signer = new System.Security.Cryptography.Pkcs.CmsSigner(signCertificate)
+            {
+                DigestAlgorithm = GetSignatureAlgorithmForCert(signCertificate),
+                IncludeOption = X509IncludeOption.EndCertOnly
+            };
+            if (requestTimestamp.HasValue)
+                signer.SignedAttributes.Add(new System.Security.Cryptography.Pkcs.Pkcs9SigningTime(requestTimestamp.Value));
+            signedCms.ComputeSignature(signer);
+            return signedCms.Encode();
+        }
+
+        private static byte[] EncryptData(byte[] data, X509Certificate encryptionCertificate)
+        {
+            var dataGenerator = new CmsEnvelopedDataGenerator();
+            dataGenerator.AddKeyTransRecipient(encryptionCertificate);
+            var encryptedData = dataGenerator.Generate(new CmsProcessableByteArray(data), CmsEnvelopedGenerator.Aes256Cbc);
+            return encryptedData.GetEncoded();
+        }
+
+        private static Oid GetSignatureAlgorithmForCert(X509Certificate2 cert)
+        {
+            switch (cert.SignatureAlgorithm.FriendlyName)
+            {
+                case "sha256RSA":
+                    return _hashSha256;
+                case "sha1RSA":
+                    return _hashSha1;
+            }
+            throw new ExtraException($"Unsupported signature algorithm {cert.SignatureAlgorithm.FriendlyName}");
+        }
+    }
+}
+#endif
--- src/Dataline.ExtraStandard.Encryption/Pkcs7EncryptionHandler.cs
+++ src/Dataline.ExtraStandard.Encryption/Pkcs7EncryptionHandler.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
+#if !NET45
 using Org.BouncyCastle.Asn1;
 using Org.BouncyCastle.Asn1.Cms;
 using Org.BouncyCastle.Asn1.Pkcs;
 using Org.BouncyCastle.Cms;
+#endif
 using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.Security;
-using Org.BouncyCastle.X509;
+
+using X509Certificate = Org.BouncyCastle.X509.X509Certificate;
 
 namespace ExtraStandard.Encryption
 {
@@ -18,9 +22,7 @@ namespace ExtraStandard.Encryption
     /// </summary>
     public class Pkcs7EncryptionHandler : IExtraEncryptionHandler
     {
-        private readonly IEnumerable<Pkcs12Store> _allSenderCertificates;
-        private readonly Pkcs12Store _senderCertificate;
-        private readonly X509Certificate _receiverCertificate;
+        private readonly IExtraEncryptionHandler _encryptionHandler;
 
         /// <summary>
         /// Initialisiert eine neue Instanz der <see cref="Pkcs7EncryptionHandler"/> Klasse.
@@ -28,13 +30,44 @@ namespace ExtraStandard.Encryption
         /// <param name="senderCertificate">Das X509-Zertifikat des Absenders (PKCS#12)</param>
         /// <param name="receiverCertificate">Das X509-Zertifikat des Empfängers</param>
         /// <param name="oldSenderCertificates">Die abgelaufenen X509-Zertifikate des Absenders</param>
-        public Pkcs7EncryptionHandler(Pkcs12Store senderCertificate, X509Certificate receiverCertificate,
-            IEnumerable<Pkcs12Store> oldSenderCertificates = null)
+        public Pkcs7EncryptionHandler(Pkcs12Store senderCertificate, X509Certificate receiverCertificate, IEnumerable<Pkcs12Store> oldSenderCertificates = null)
         {
-            _senderCertificate = senderCertificate;
-            _receiverCertificate = receiverCertificate;
-            _allSenderCertificates = (oldSenderCertificates ?? new Pkcs12Store[0]).Concat(new[] { senderCertificate }).Reverse().ToList();
+#if NET45
+            var senderCert = ConvertToCertificate2(senderCertificate);
+            var recCert = ConvertToCertificate2(receiverCertificate);
+            var oldSenderCerts = oldSenderCertificates?.Select(ConvertToCertificate2);
+            _encryptionHandler = new NativePkcs7EncryptionHandler(senderCert, recCert, oldSenderCerts);
+#else
+            _encryptionHandler = new BouncyCastlePkcs7EncryptionHandler(senderCertificate, receiverCertificate, oldSenderCertificates);
+#endif
         }
+
+        /// <summary>
+        /// Initialisiert eine neue Instanz der <see cref="Pkcs7EncryptionHandler"/> Klasse.
+        /// </summary>
+        /// <param name="senderCertificate">Das X509-Zertifikat des Absenders (PKCS#12)</param>
+        /// <param name="receiverCertificate">Das X509-Zertifikat des Empfängers</param>
+        /// <param name="oldSenderCertificates">Die abgelaufenen X509-Zertifikate des Absenders</param>
+        public Pkcs7EncryptionHandler(X509Certificate2 senderCertificate, X509Certificate2 receiverCertificate, IEnumerable<X509Certificate2> oldSenderCertificates = null)
+        {
+#if NET45
+            _encryptionHandler = new NativePkcs7EncryptionHandler(senderCertificate, receiverCertificate, oldSenderCertificates);
+#else
+            var senderCert = new Pkcs12Store(new MemoryStream(senderCertificate.Export(X509ContentType.Pkcs12)), new char[0]);
+            var receiverCert = new Org.BouncyCastle.X509.X509CertificateParser().ReadCertificate(receiverCertificate.RawData);
+            var oldSenderCerts = oldSenderCertificates?.Select(cert => new Pkcs12Store(new MemoryStream(cert.Export(X509ContentType.Pkcs12)), new char[0]));
+            _encryptionHandler = new BouncyCastlePkcs7EncryptionHandler(senderCert, receiverCert, oldSenderCerts);
+#endif
+        }
+
+        /// <summary>
+        /// Wird native Signatur und Entschlüsselung unterstützt?
+        /// </summary>
+#if NET45
+        public static bool IsNativeSupported { get; } = true;
+#else
+        public static bool IsNativeSupported { get; } = false;
+#endif
 
         /// <inheritdoc />
         public string AlgorithmId { get; } = ExtraEncryption.Pkcs7;
@@ -42,107 +75,27 @@ namespace ExtraStandard.Encryption
         /// <inheritdoc />
         public byte[] Encrypt(byte[] data, DateTime requestTimestamp)
         {
-            var signedData = SignData(data, _senderCertificate, requestTimestamp);
-            var encryptedData = EncryptData(signedData, _receiverCertificate);
-            return encryptedData;
+            return _encryptionHandler.Encrypt(data, requestTimestamp);
         }
 
         /// <inheritdoc />
         public byte[] Decrypt(byte[] data)
         {
-            foreach (var pkcsStore in _allSenderCertificates)
-            {
-                var certAlias = pkcsStore.Aliases.Cast<string>().First(x => pkcsStore.IsKeyEntry(x));
-                var certEntry = pkcsStore.GetCertificate(certAlias);
-                var cert = certEntry.Certificate;
-
-                var envelopedData = new CmsEnvelopedData(data);
-                var recepientInfos = envelopedData.GetRecipientInfos();
-                var recepientId = new RecipientID()
-                {
-                    Issuer = cert.IssuerDN,
-                    SerialNumber = cert.SerialNumber
-                };
-                var recepient = recepientInfos[recepientId];
-                if (recepient == null)
-                    continue;
-
-                var privKeyEntry = pkcsStore.GetKey(certAlias);
-                var privKey = privKeyEntry.Key;
-
-                var decryptedData = recepient.GetContent(privKey);
-                var sig = new CmsSignedData(decryptedData);
-                var sigInfos = sig.GetSignerInfos();
-                var signerId = new SignerID()
-                {
-                    Issuer = _receiverCertificate.IssuerDN,
-                    SerialNumber = _receiverCertificate.SerialNumber
-                };
-                var signer = sigInfos.GetFirstSigner(signerId);
-                if (!signer.Verify(_receiverCertificate))
-                    throw new ExtraEncryptionException("Failed to verify the signature.");
-
-                var verifiedData = new MemoryStream();
-                sig.SignedContent.Write(verifiedData);
-
-                return verifiedData.ToArray();
-            }
-
-            throw new ExtraEncryptionException("No certificate for decryption found.");
+            return _encryptionHandler.Decrypt(data);
         }
 
-        private static byte[] SignData(byte[] data, Pkcs12Store signCertificate, DateTime? requestTimestamp = null)
+#if NET45
+        private static X509Certificate2 ConvertToCertificate2(Pkcs12Store store)
         {
-            var signCertAlias = signCertificate.Aliases.Cast<string>().First(signCertificate.IsKeyEntry);
-            var signCertEntry = signCertificate.GetCertificate(signCertAlias);
-            var signCert = signCertEntry.Certificate;
-            var signPkEntry = signCertificate.GetKey(signCertAlias);
-            var signPk = signPkEntry.Key;
-            string digestName;
-
-            if (signCert.SigAlgOid == PkcsObjectIdentifiers.Sha1WithRsaEncryption.Id)
-            {
-                digestName = "SHA1";
-            }
-            else if (signCert.SigAlgOid == PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id)
-            {
-                digestName = "SHA256";
-            }
-            else
-            {
-                throw new ExtraException($"Unsupported digest algorithm {signCert.SigAlgName}");
-            }
-
-            var digestOid = DigestUtilities.GetObjectIdentifier(digestName).Id;
-            var digest = DigestUtilities.CalculateDigest(digestName, data);
-
-            var signedAttrs = new Dictionary<object, object>()
-            {
-                { CmsAttributeTableParameter.Digest, digest }
-            };
-
-            if (requestTimestamp.HasValue)
-            {
-                var signTimestamp = new Org.BouncyCastle.Asn1.Cms.Attribute(CmsAttributes.SigningTime, new DerSet(new Time(requestTimestamp.Value.ToUniversalTime())));
-                signedAttrs.Add(signTimestamp.AttrType, signTimestamp);
-            }
-
-            var signedAttrGen = new DefaultSignedAttributeTableGenerator();
-            var signedAttrTable = signedAttrGen.GetAttributes(signedAttrs);
-
-            var generator = new CmsSignedDataGenerator();
-            generator.AddSigner(signPk, signCert, digestOid, new DefaultSignedAttributeTableGenerator(signedAttrTable), null);
-
-            var signedData = generator.Generate(new CmsProcessableByteArray(data), true);
-            return signedData.GetEncoded();
+            var senderCertStream = new MemoryStream();
+            store.Save(senderCertStream, new char[0], new SecureRandom());
+            return new X509Certificate2(senderCertStream.ToArray());
         }
 
-        private static byte[] EncryptData(byte[] data, X509Certificate encryptionCertificate)
+        private static X509Certificate2 ConvertToCertificate2(X509Certificate cert)
         {
-            var dataGenerator = new CmsEnvelopedDataGenerator();
-            dataGenerator.AddKeyTransRecipient(encryptionCertificate);
-            var encryptedData = dataGenerator.Generate(new CmsProcessableByteArray(data), CmsEnvelopedGenerator.Aes256Cbc);
-            return encryptedData.GetEncoded();
+            return new X509Certificate2(cert.GetEncoded());
         }
+#endif
     }
 }
