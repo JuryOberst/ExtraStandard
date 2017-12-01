6f650fac0c41fbbde02a2dfda255df92321a2417
Mark Junker <m.junker@fubar-dev.de>
2017-12-01 11:44:44 +0100

    Überflüssige Dateien entfernt

--- build.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set BUILD_CFG=%1
-if "%BUILD_CFG%" equ "" set BUILD_CFG=Debug
-dotnet restore
-if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet build src/Dataline.ExtraStandard*/project.json -c %BUILD_CFG% --version-suffix "%2"
--- pack.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set BUILD_CFG=%1
-if "%BUILD_CFG%" equ "" set BUILD_CFG=Debug
-if exist "package-output" del "package-output" /f /q
-for /d %%D in (src\Dataline.ExtraStandard*) do (
-	dotnet pack %%D -c %BUILD_CFG% -o "package-output" --no-build --version-suffix "%2"
-	if %errorlevel% neq  0 exit /b %errorlevel%
-)
